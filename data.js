3339c2f7c814d0db81d0119d891152f917e6e07b
Bruno M Noronha <bruno.developer@pm.me>
2020-05-10 21:11:46 -0300

    finalizando tray

--- data.js
+++ data.js
@@ -19,18 +19,26 @@ module.exports = {
         let fullFileName = __dirname + '/data/' + course + '.json';
         return jsonfile.readFile(fullFileName);
     },
+    getCoursesName() {
+
+        let files = fs.readdirSync(__dirname + '/data/');
+        let courses = files.map((file) => {
+            return file.substr(0, file.lastIndexOf('.'));
+        })
+        return courses;
+    },
     addTimeToCourse(fullFileName, studyDuration) {
 
         let data = {
             updateAt: new Date().toString(),
             time: studyDuration
         }
-        jsonfile.writeFile(fullFileName, data, {spaces: 2})
-        .then(() => {
-            console.log('dados incluidos com sucesso');
-        }).catch((error) => {
-            console.log(error);
-        })
+        jsonfile.writeFile(fullFileName, data, { spaces: 2 })
+            .then(() => {
+                console.log('dados incluidos com sucesso');
+            }).catch((error) => {
+                console.log(error);
+            })
 
     },
     createFile(fileName, fileContent) {
